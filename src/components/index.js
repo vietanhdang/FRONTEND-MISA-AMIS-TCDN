283bd27a7587ce5de183b96ddfa620dc88fccaed
Viet Anh Dang <40699842+vietanhdang@users.noreply.github.com>
2022-09-23 10:17:46 +0700

    feat: Thêm một vài component dùng chung cho toàn bộ project

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,17 @@
 // Xuất tất cả các component trong thư mục components
-import VButton from "./VButton";
-import VInput from "./VInput";
-import VSelect from "./VSelect";
-import VCombobox from "./VCombobox";
-import VTooltip from "./VTooltip";
-import VToast from "./VToast";
-import VDialog from "./VDialog";
-import VTable from "./VTable";
-import VDatePicker from "./VDatePicker";
+import VButton from "./base/VButton";
+import VInput from "./base/VInput";
+import VSelect from "./base/VSelect";
+import VCombobox from "./base/VCombobox";
+import VTooltip from "./base/VTooltip";
+import VToast from "./base/VToast";
+import VDialog from "./base/VDialog";
+import VTable from "./base/VTable";
+import VModal from "./base/VModal";
+import VPopup from "./base/VPopup";
+import VDatePicker from "./base/VDatePicker";
+import TheHeader from "./layout/TheHeader";
+import TheSidebar from "./layout/TheSidebar";
 export default {
   VButton,
   VInput,
@@ -18,4 +22,8 @@ export default {
   VDialog,
   VDatePicker,
   VTable,
+  TheHeader,
+  TheSidebar,
+  VModal,
+  VPopup,
 };
