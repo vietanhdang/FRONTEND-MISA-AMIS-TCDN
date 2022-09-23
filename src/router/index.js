ebd0395c9e2d03cad96abd7a32c5c72e8f0fb413
Viet Anh Dang <40699842+vietanhdang@users.noreply.github.com>
2022-09-23 10:16:35 +0700

    feat: thêm tính năng Lazy loading để tối ưu lại tốc độ tải trang

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,30 @@
-import CompEmployeeList from "@/views/employee/EmployeeList";
-import CompTestVueRouter from "@/views/employee/TestVueRouter";
+import { createRouter, createWebHistory } from "vue-router";
+
+function lazyLoad(view) {
+  return () => import(`@/views/${view}.vue`);
+}
 
 // Khai báo các routes
 const router = [
   {
     path: "/nhan-vien",
-    component: CompEmployeeList,
+    component: lazyLoad("employee/EmployeeList"),
     meta: {
       title: "Danh sách nhân viên",
     },
   },
-  {
-    path: "/khach-hang",
-    component: CompTestVueRouter,
-    meta: {
-      title: "Danh sách khách hàng",
-    },
-  },
 ];
 
-export default router;
+// Khởi tạo router
+const routes = createRouter({
+  history: createWebHistory(),
+  routes: router,
+});
+
+// Cập nhật meta title cho từng route
+routes.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
+});
+
+export default routes;
