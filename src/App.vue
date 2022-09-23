b48245cc282802e3e8701ab8130b448918817c5c
Viet Anh Dang <40699842+vietanhdang@users.noreply.github.com>
2022-09-23 10:17:09 +0700

    feat: Cập nhật lại cấu trúc header, sidebar ,content của web

--- src/App.vue
+++ src/App.vue
@@ -10,23 +10,11 @@
       </div>
     </div>
   </div>
-  <!-- <div class="main">
-      <div class="content" :class="{ 'content--collapsed': collapsed }">
-      </div>
-    </div> -->
-
 </template>
 
 <script>
-// import EmployeeList from './components/content/EmployeeList.vue'
-import TheHeader from '@/layout/header/TheHeader.vue'
-import TheSidebar from '@/layout/sidebar/TheSidebar.vue'
 export default {
   name: "App",
-  components: {
-    TheHeader,
-    TheSidebar,
-  },
   data() {
     return {
       collapsed: false
