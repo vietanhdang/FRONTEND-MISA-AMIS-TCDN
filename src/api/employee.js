182fabe08d425f384519d2585d2aa3f5a9727b3b
Viet Anh Dang <40699842+vietanhdang@users.noreply.github.com>
2022-09-23 10:18:00 +0700

    feat: Cập nhật lại các API của nhân viên

--- src/api/employee.js
+++ src/api/employee.js
@@ -1,14 +1,14 @@
 export default (axios) => ({
   getNewEmployeeCode() {
     return axios.get(
-      process.env.VUE_APP_API_URL + "/Employees/NewEmployeeCode"
+      process.env.VUE_APP_API_URL + "/employees/new-employee-code"
     );
   },
 
   getEmployeeById(id) {
-    return axios.get(process.env.VUE_APP_API_URL + "/Employees/" + id);
+    return axios.get(process.env.VUE_APP_API_URL + "/employees/" + id);
   },
-  getAllEmployees(object) {
+  getEmployeesFilter(object) {
     var defaultObject = {
       pageNumber: 1,
       pageSize: 10,
@@ -16,7 +16,7 @@ export default (axios) => ({
       sort: "",
     };
     object = Object.assign(defaultObject, object);
-    var url = process.env.VUE_APP_API_URL + "/Employees/Filter";
+    var url = process.env.VUE_APP_API_URL + "/employees/filter";
     url += "?pageNumber=" + object.pageNumber;
     url += "&pageSize=" + object.pageSize;
     url += "&search=" + object.search;
@@ -24,14 +24,14 @@ export default (axios) => ({
     return axios.get(url);
   },
   deleteEmployee(id) {
-    return axios.delete(process.env.VUE_APP_API_URL + "/Employees/" + id);
+    return axios.delete(process.env.VUE_APP_API_URL + "/employees/" + id);
   },
   insertEmployee(object) {
-    return axios.post(process.env.VUE_APP_API_URL + "/Employees", object);
+    return axios.post(process.env.VUE_APP_API_URL + "/employees", object);
   },
   updateEmployee(object) {
     return axios.put(
-      process.env.VUE_APP_API_URL + "/Employees/" + object.employeeID,
+      process.env.VUE_APP_API_URL + "/employees/" + object.employeeID,
       object
     );
   },
