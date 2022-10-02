f7933ef03da82e7703450994e993862596ea0fcf
Viet Anh Dang <40699842+vietanhdang@users.noreply.github.com>
2022-10-02 20:25:17 +0700

    feat: Thêm trang tính năng đang cập nhật

---
+++ src/views/update/UpdatePage.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="develop-page">
+        <div class="develop-page__content">
+            <div class="develop-page__content__title">
+                <img :src="require('@/assets/img/development.svg')" alt="error" />
+            </div>
+            <div class="develop-page__content__description">
+                <h1 class="font-weight">Tính năng này đang được phát triển</h1>
+            </div>
+            <div class="develop-page__content__button d-flex justify-content-center border-rounded">
+                <router-link to="/">
+                    <v-button className="border-radius-30">
+                        Quay lại
+                    </v-button>
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.develop-page {
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #fff;
+
+    .develop-page__content {
+        transform: translateY(-48px);
+        text-align: center;
+
+        .landing-page__content__title {
+            img {
+                width: 100%;
+                max-width: 400px;
+            }
+        }
+
+        .develop-page__content__description {
+            margin: 32px;
+            color: #111;
+        }
+    }
+}
+</style>
